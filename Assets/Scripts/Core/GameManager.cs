2ece350c854053a0e7d83ce60340036ddb9fd360
Rull Deef <deeroll666@gmail.com>
2021-01-19 17:53:01 +0000

    init 3d version

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public new Camera camera;
+    private Plane plane;
+
+    private LockMover lockMover;
+
+    public Vector3 lastMousePosition;
+
+    private void Start()
+    {
+        plane = new Plane(Vector3.up, 0);
+
+        lockMover = GetComponent<LockMover>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            lockMover.HoldRing(0);
+        else if (Input.GetMouseButtonUp(0))
+            lockMover.DisposeRing();
+
+        if (lockMover.IsHolding())
+        {
+            Vector3 currMousePosition = RaycastMousePosition(Input.mousePosition);
+            float lastAngle = Mathf.Atan2(lastMousePosition.z, lastMousePosition.x);
+            float currAngle = Mathf.Atan2(currMousePosition.z, currMousePosition.x);
+
+            float deltaAngle = currAngle - lastAngle;
+            lockMover.UpdateRingAngle(deltaAngle);
+        }
+
+        lastMousePosition = RaycastMousePosition(Input.mousePosition);
+    }
+
+    private Vector3 RaycastMousePosition(Vector3 position)
+    {
+        Ray ray = camera.ScreenPointToRay(position);
+        Vector3 hitPoint = Vector3.zero;
+
+        float enter = 0.0f;
+        if (plane.Raycast(ray, out enter))
+            hitPoint = ray.GetPoint(enter);
+
+        return hitPoint;
+    }
+}
---
+++ Assets/Scripts/Core/LockMover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMover : MonoBehaviour
+{
+    public Transform lockTransform;
+    public int ringIndex;
+
+    public Transform[] layers;
+
+    void Start()
+    {
+        int layersCount = lockTransform.childCount - 1;
+        layers = new Transform[layersCount];
+
+        for (int i = 0; i < layersCount; i++)
+        {
+            layers[i] = lockTransform.Find($"Layer{i}");
+            if (layers[i] == null)
+                Debug.LogError("Invalid lock structure!");
+        }
+
+        ringIndex = -1;
+    }
+
+    public bool IsHolding()
+    {
+        return ringIndex != -1;
+    }
+
+    public void HoldRing(int ringIndex)
+    {
+        if (this.ringIndex == -1)
+            this.ringIndex = ringIndex;
+        else
+            Debug.LogError("Try to hold before dispose");
+    }
+
+    public void UpdateRingAngle(float deltaAngle)
+    {
+        if (ringIndex == -1)
+            Debug.LogError("Invalid ring index");
+        else
+        {
+            layers[ringIndex].Rotate(-180.0f / Mathf.PI * deltaAngle * Vector3.up);
+        }
+    }
+
+    public void DisposeRing()
+    {
+        this.ringIndex = -1;
+    }
+}
